165ed3a263e967c1270cfbba66e7ea10e34bf34b
LouizD <louise.dupas@gmail.com>
2021-10-18 15:54:21 +0200

    feat: create dummy components for testing

---
+++ src/components/title/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Title = props => {
+  return (
+    <Text>{props.children}</Text>
+  )
+}
+
+export default Title;
---
+++ src/screens/Test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Title } from '../components/title/Title';
+
+const TestScreen = props => {
+  return (
+    <Title>Awesome title</Title>
+  )
+}
+
+export default TestScreen;
