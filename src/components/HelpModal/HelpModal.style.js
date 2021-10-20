38ea4587ee32efd9041fd70a69d584e4d69b12f6
LouizD <louise.dupas@gmail.com>
2021-10-20 17:17:59 +0200

    fix: path to var for HelpModal style file

--- src/components/HelpModal/HelpModal.style.js
+++ src/components/HelpModal/HelpModal.style.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { colors } from '../_var';
+import { colors } from '../../_var';
 
 const styleSheet = StyleSheet.create({
   container: {
